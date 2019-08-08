d2fb850ad9abe7d61244e0a689e74035bbf11e07
Pablo Curell <pablocm90@gmail.com>
2019-08-08 17:46:01 +0200

    fix prod?

--- app/javascript/packs/vue.js
+++ app/javascript/packs/vue.js
@@ -4,9 +4,6 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
 
-// css
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 
